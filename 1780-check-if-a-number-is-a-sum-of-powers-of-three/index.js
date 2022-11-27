466147d49a36e3b820f8f48628c11a3e4022d0ea
Wojciech Komor <wojtekkomor@gmail.com>
2022-11-27 16:04:54 +0100

    1780 sum of powers of threE

---
+++ 1780-check-if-a-number-is-a-sum-of-powers-of-three/index.js
@@ -0,0 +1,7 @@
+/*
+* 1780. Check if Number is a Sum of Powers of Three
+* https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/
+*
+*/
+
+const checkPowersOfThree = n => !n.toString(3).includes('2');
