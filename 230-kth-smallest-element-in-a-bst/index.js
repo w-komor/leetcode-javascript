11d01f5dad07cd4a48ebf39a5e03d408dcb3e423
Wojciech Komor <wojtekkomor@gmail.com>
2022-12-06 23:13:51 +0100

    expanation for problem 230

--- 230-kth-smallest-element-in-a-bst/index.js
+++ 230-kth-smallest-element-in-a-bst/index.js
@@ -2,6 +2,9 @@
 * 230. Kth Smallest Element in a BST
 * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 *
+* In-order traversal of a BST yields the elements in non-decreasing order.
+* We just return the kth smallest element of the in-order traversal.
+*
 */
 
 const inOrder = node => node ? [...inOrder(node.right), node.val, ...inOrder(node.left)] : [];
