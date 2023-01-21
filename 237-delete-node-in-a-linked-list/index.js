9c6ebeab86329604655e7982c09b8156ed06c884
Wojciech Komor <wojtekkomor@gmail.com>
2023-01-21 20:11:43 +0100

    237 delete node in a linked list

---
+++ 237-delete-node-in-a-linked-list/index.js
@@ -0,0 +1,10 @@
+/*
+* 237. Delete Node in a Linked List
+* https://leetcode.com/problems/delete-node-in-a-linked-list/
+*
+*/
+
+const deleteNode = node => {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
