36b20895b7ca757f6a31933b3823f3da80bcb958
Wojciech Komor <wojtekkomor@gmail.com>
2023-03-09 09:15:06 +0100

    corrected solution for 142

--- 142-linked-list-cycle-ii/index.js
+++ 142-linked-list-cycle-ii/index.js
@@ -4,17 +4,21 @@
 *
 */
 
-const detectCycle = head => {
-    let fast = head,
-        slow = head,
-        prev = null;
+var detectCycle = function(head){
+    let slow = head,
+        fast = head;
 
-    while (fast && fast.next) {
-        prev = prev === null ? head : prev.next;
+    while (fast && fast.next && fast.next.next) {
         slow = slow.next;
         fast = fast.next.next;
-        if (slow === fast) return prev;
+        if (slow !== fast) continue;
+        slow = head;
+        while (slow !== fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 
     return null;
-};
+}
